6cb8ae19db4288c6b16feaf95d4b4d3eb1bf1b7a
plasticine <975036719@qq.com>
2023-05-24 21:27:39 +0800

    feat(playground/jsx-runtime): 用 @plasticine-react/react 测试 jsx 转换结果

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -2,6 +2,7 @@ const { readdirSync } = require('fs')
 const { resolve } = require('path')
 
 const packages = readdirSync(resolve(__dirname, 'packages'))
+const playgrounds = readdirSync(resolve(__dirname, 'playgrounds')).map((item) => `playground/${item}`)
 
 /**
  * @type { import('cz-git').UserConfig }
@@ -12,6 +13,6 @@ module.exports = {
     'subject-case': [2, 'never', ['upper-case']],
   },
   prompt: {
-    scopes: [...packages],
+    scopes: [...packages, ...playgrounds],
   },
 }
---
+++ playgrounds/jsx-runtime/src/main.tsx
@@ -0,0 +1,7 @@
+const foo = (
+  <div id="foo" className="foo">
+    foo
+  </div>
+)
+
+console.log(foo)
